54ef2f76ca22f5c09dbc81e17a9e2869f31e0327
fcmwf <1298154821@qq.com>
2023-06-04 11:44:55 +0800

    lab5

---
+++ lab5/myOS/include/string.h
@@ -0,0 +1,7 @@
+#ifndef __STRING_H__
+#define __STRING_H__
+
+int Mystrcmp(unsigned char *str1, unsigned char *str2);
+int Mystrncpy(unsigned char *src, unsigned char *dest, unsigned int n);
+
+#endif
